c37f8610c7c4bf55486665826504cc2e0dadd5e4
samuelmalik <samuelmalik9@gmail.com>
2022-09-21 21:22:04 +0200

    added function comparing two strings regardless of their case

--- Open-Lab-03.05/Comparator.cs
+++ Open-Lab-03.05/Comparator.cs
@@ -6,7 +6,7 @@ namespace Open_Lab_03._05
     {
         public bool MatchCaseInsensitive(string str1, string str2)
         {
-            throw new NotImplementedException();
+            return str1.ToLower() == str2.ToLower();
         }
     }
 }
